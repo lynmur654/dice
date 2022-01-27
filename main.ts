1b6128872a68cea81a40026a6f53d55f229c1def
lynmur654 <98560653+lynmur654@users.noreply.github.com>
2022-01-27 14:48:23 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let idk = 0
+input.onGesture(Gesture.Shake, function () {
+    idk = 0
+    if (true) {
+        if (true) {
+            radio.sendValue("name", 0)
+        } else {
+            music.ringTone(262)
+        }
+        basic.showNumber(randint(1, 6))
+    }
+})
